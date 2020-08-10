b85cc3fb2fdc8232288bdf9f06fdbf2782ff987c
samuelematias <hi@samuelematias.com>
2020-08-10 16:58:52 -0300

    feat(async): implemented Future async/await

--- lib/main.dart
+++ lib/main.dart
@@ -13,54 +13,32 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            print('Future started');
+            // You use AWAIT option, when you want
+            // to wait for the future to complete
+            // before continuing the execution.
+            // The dart then call is used when your
+            // code can continue executing after
+            // the future has been called.
+            var futureValue = await myFuture();
+            print(futureValue);
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
     );
   }
+
+  // With async/await
+  Future<String> myFuture() async {
+    await Future.delayed(Duration(seconds: 1));
+    return 'This future is complete';
+  }
+
+  // Without async/await
+  //   Future<String> myFuture() {
+  //   return Future.value('This future is complete');
+  // }
 }
