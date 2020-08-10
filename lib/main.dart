98e9b160be60cd0a329b50e315a26f84a607873f
samuelematias <hi@samuelematias.com>
2020-08-10 17:05:41 -0300

    feat(future): using Future then

--- lib/main.dart
+++ lib/main.dart
@@ -15,17 +15,34 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            print('Future started');
-            // You use AWAIT option, when you want
-            // to wait for the future to complete
-            // before continuing the execution.
-            // The dart then call is used when your
-            // code can continue executing after
-            // the future has been called.
-            var futureValue = await myFuture();
-            print(futureValue);
+          //With THEN
+          onPressed: () {
+            print('Future started - THEN');
+            //The difference between this (using THEN)
+            // and the async/await, is that
+            // the function will not wait for the
+            // Future to complete to continue executing.
+            // It does not wait for the execution
+            // of a Future to finish
+            // before completing the function.
+            myFuture().then((value) {
+              print('Future finished - THEN');
+            });
+            print('Future now here');
           },
+
+          // With async/await
+          // onPressed: () async {
+          //   print('Future started');
+          //   // You use AWAIT option, when you want
+          //   // to wait for the future to complete
+          //   // before continuing the execution.
+          //   // The dart then call is used when your
+          //   // code can continue executing after
+          //   // the future has been called.
+          //   var futureValue = await myFuture();
+          //   print(futureValue);
+          // },
         ),
       ),
     );
